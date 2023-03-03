24d3ba2de03bac92bd479b17f9acef93596eda75
Chiaravalle04 <nicolas.chiaravalle@gmail.com>
2023-03-03 15:42:39 +0100

    Completato BONUS 1

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
         public $description;
         public $watched;
 
-        function __construct(string $_title, string $_genre, string $_description, bool $_watched = false) {
+        function __construct(string $_title, string $_description, array $_genre = array(), bool $_watched = false) {
             
             $this->title = $_title;
             $this->genre = $_genre;
@@ -20,19 +20,23 @@
 
             echo "<h2>Film</h2>";
             echo "<b>Titolo:</b> " . $this->title . "<br>";
-            echo "<b>Genere:</b> " . $this->genre . "<br>";        
             echo "<b>Descrizione:</b> " . $this->description . "<br>";        
+            echo "<b>Genere:</b> ";
+            if (empty($this->genre)) {
+                echo " "."<br>";
+            } else {
+                echo implode(', ', $this->genre) . "<br>";
+            } 
             echo "<b>Visto:</b> " . ($this->watched ? "Si" : "No") . "<br>";
+            
         }
     }
 
-    $pulpFiction = new Movie('Pulp Fiction', 'Giallo', 'Il film è diviso in diverse storie interconnesse che ruotano attorno al mondo della criminalità organizzata di Los Angeles.', true);
-
-    var_dump($pulpFiction);
+    $pulpFiction = new Movie('Pulp Fiction', 'Il film è diviso in diverse storie interconnesse che ruotano attorno al mondo della criminalità organizzata di Los Angeles.', array('Giallo', 'Drammatico'), true);
 
     $pulpFiction->printMovie();
 
-    $djangoUnchained = new Movie('Django Unchained', 'Western', 'La D è muta!', true);
+    $djangoUnchained = new Movie('Django Unchained', 'La D è muta!', array('Western'), true);
 
     $djangoUnchained->printMovie();
 
